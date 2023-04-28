c365de06c20b3bc484788f713d4094b4e47b3edf
Prop4et <francostanco97@gmail.com>
2023-04-28 15:44:34 +0200

    started working on the main

--- src/executables/pico-ph/main.c
+++ src/executables/pico-ph/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
-#include "ph_driver.h"
+#include "../../lib/ph_driver/ph_driver.h"
+#include "../../lib/sht21/sht21.h"
+#include "../../lib/motor/motor_driver.h"
 
 int main(){
     stdio_init_all();
     struct ph_driver ph;
-    ph_init(&ph);
+    struct motor m;
+    motor_init(&m, -1, -1, -1);
+    ph_init(&ph, NULL, -1, -1);
+    sht21_init(-1, -1);
+    
 }

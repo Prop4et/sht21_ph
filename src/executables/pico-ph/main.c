1bc2232514d47a43610c9682381e30db613f0116
Prop4et <francostanco97@gmail.com>
2023-04-28 16:12:18 +0200

    added packet and savin readings

--- src/executables/pico-ph/main.c
+++ src/executables/pico-ph/main.c
@@ -5,12 +5,37 @@
 #include "../../lib/sht21/sht21.h"
 #include "../../lib/motor/motor_driver.h"
 
+#define DEV_ID               8
+#define CHECKMASK            UINT8_C(1)
+
+struct uplink {
+    uint16_t id; 
+    float temp;
+    float hum;
+    float ph[3];
+};
+
 int main(){
     stdio_init_all();
     struct ph_driver ph;
     struct motor m;
+    struct uplink pkt = {
+        .id = DEV_ID
+    };
+
     motor_init(&m, -1, -1, -1);
     ph_init(&ph, NULL, -1, -1);
     sht21_init(-1, -1);
-    
+    float ph_buf[N_SENS] = {0, 0, 0};
+    pkt.temp = sht21_temperature();
+    pkt.hum = sht21_humidity();
+    uint8_t bitmask = ph_get(ph, m, ph_buf);
+    for(int i = 0; i<N_SENS; i++){
+        if(bitmask & CHECKMASK)
+            pkt.ph[i] = ph_buf[i];
+        else
+            pkt.ph[i] = -1000;
+        bitmask = bitmask >> 1;
+    }
+    //TODO: LoRa protocol and check the pins used
 }
